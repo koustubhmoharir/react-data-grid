3354c1a1ae2448cd5cad0311d31c74e37f5c7676
Koustubh Moharir <koustubhmoharir@gmail.com>
2018-01-06 13:54:49 +0530

    Fixed missing sk suffix in imports

--- packages/react-data-grid-examples/src/scripts/example27-cell-actions.js
+++ packages/react-data-grid-examples/src/scripts/example27-cell-actions.js
@@ -1,5 +1,5 @@
 const faker = require('faker');
-const ReactDataGrid = require('react-data-grid');
+const ReactDataGrid = require('react-data-grid-sk');
 const exampleWrapper = require('../components/exampleWrapper');
 const React = require('react');
 const {
@@ -7,7 +7,7 @@ const {
   Data: { Selectors },
   Draggable: { Container: DraggableContainer },
   Formatters: { ImageFormatter }
-} = require('react-data-grid-addons');
+} = require('react-data-grid-sk-addons');
 
 import PropTypes from 'prop-types';
 
